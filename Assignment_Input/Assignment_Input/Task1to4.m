ed8696573b33a0e3b622b3df35872a1688342dac
Mathews Joy <mathews2492@gmail.com>
2023-11-18 22:16:52 +0000

    added tasks 2-3

--- Assignment_Input/Assignment_Input/Task1to4.m
+++ Assignment_Input/Assignment_Input/Task1to4.m
@@ -26,19 +26,22 @@ ma = max(max(J)); % find the maximum pixel intensity
 I_gray_scale_bi_enhanced = imadjust(I_gray_scale_bi,[mi/255; ma/255],[0; 1]);
 
 % Display the enhanced image
-figure, imshow(I_gray_scale_bi_enhanced);
+figure; imshow(I_gray_scale_bi_enhanced);
 
 % Step-6: Histogram after enhancement
 histogram(I_gray_scale_bi_enhanced);
 title("Step-6: Histogram for after enhancement.");
 
 
+
 % Step-7: Image Binarisation
-threshold = double(120/255); 
+threshold = double(125/255); 
 
 binarisedImage = imbinarize(I_gray_scale_bi_enhanced, threshold);
 figure, imshow(binarisedImage)
 title("Step-5: Producing binarised image")
+
+
 
 
 % Display the re-sized image, histograms before and after enhancement,
@@ -62,45 +65,71 @@ pos = get(gcf, 'Position'); % gives the position of current sub-plot
 set(gcf, 'Position',pos+[0 -100 100 100]) % set new position of current sub - plot
 
 
-
 % Task 2: Edge detection ------------------------
 
-% Experiment with the 3 smoothing methods below which one give better edge
-% detecetion results or neither? to reduce any noise
-% Smooth the image using a Gaussian filter (test cahnign the standard
-% deivation)
-I_gray_scale_bi_enhanced = imgaussfilt(I_gray_scale_bi_enhanced, 1.5);
-% Smooth the image using a median filter
+% Use median filtering to help reduce noise in iamge before
+% applying edge detection
 img_smooth = medfilt2(I_gray_scale_bi_enhanced);
-% Smooth the image using a bilateral filter
-img_smooth = imbilatfilt(I_gray_scale_bi_enhanced);
 
-edgeDetectionSobel = edge(I_gray_scale_bi_enhanced,'sobel');
+% Test out sobel method for edge detection
+edgeDetectionSobel = edge(img_smooth,'sobel');
 figure;
 imshow(edgeDetectionSobel)
 title("Task 2: Edge Detection - Sobel")
 
-edgeDetectionCanny = edge(I_gray_scale_bi_enhanced,'canny');
+% Test out canny method for edge detection
+% Change the sigma value to "0.08" to perform further noiser reduction
+edgeDetectionCanny = edge(img_smooth,'canny', 0.08);
 figure; 
 imshow(edgeDetectionCanny)
 title("Task 2: Edge Detection - Canny")
 
-edgeDetectionPrewitt = edge(I_gray_scale_bi_enhanced,'Prewitt');
+% Test out prewitt method for edge detection
+edgeDetectionPrewitt = edge(img_smooth,'prewitt');
 figure; 
 imshow(edgeDetectionPrewitt)
 title("Task 2: Edge Detection - Prewitt")
 
 
 % Task 3: Simple segmentation --------------------
-% Fill the holes to get binary image of the objects
-% Try get rid of smoothing / using differnt echniqwues as some of the edges
-% are not connected fulling for the objects
-filled = imfill(edgeDetectionCanny, "holes");
 
-% Remove small objects that are not screws or washers
-cleaned = bwareaopen(filled, 50); % Adjust the second parameter as needed
+% Disk shaped structuring element with a radius of 3 pixels
+se = strel("disk", 3);
 
-imshow(cleaned);
+% Use closing the image to connect all edges of objects
+I_close = imclose(edgeDetectionCanny, se);
+
+% Fill the objects holes
+filled = imfill(I_close, "holes");
+
+% Remove small objects (that cant be screw / washer)
+I_filled_segmented = bwareaopen(filled,20);
+
+% Display segmented image
+figure;
+imshow(I_filled_segmented);
+title("Task3 – Simple Segmentation");
 
 
 % Task 4: Object Recognition --------------------
+% Label the connected components in the binary image
+% Get the aspect ratio of each blob.
+props = regionprops(I_filled_segmented, 'MajorAxisLength', 'MinorAxisLength', 'Area');
+
+aMajor = [props.MajorAxisLength]
+aMinor = [props.MinorAxisLength]
+allAreas = sort([props.Area])
+aspectRatios = aMajor ./ aMinor
+numBlobs = length(props)
+cmap = zeros(numBlobs+1, 3);
+for k = 1 : numBlobs
+	if aspectRatios(k) > 2 % Whatever value you want.
+		cmap(k+1, :) = [1, 0, 0]; % Red.
+	else
+		cmap(k+1, :) = [0.9100, 0.4100, 0.1700]; % Orange.
+	end
+end
+cmap;
+labeledImage = bwlabel(I_filled_segmented);
+imshow(labeledImage, []);
+colormap(cmap);
